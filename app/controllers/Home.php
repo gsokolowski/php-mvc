3e0d76492f9a987053a26a36116ca76d1e0e40a0
Greg <greg@mac.home>
2025-09-24 23:54:50 +0200

    router and controllers done

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,7 +3,11 @@
 namespace App\Controllers;
 
 class Home {
+    
     public function index() {
-        echo "home index";
+        echo "home controller loaded and method index called";
     }
 }
+
+$home = new Home();
+$home->index(); 
