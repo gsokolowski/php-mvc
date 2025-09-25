99d545ac7013bc0e896c4edd0d162bd522d596cf
gsokolowski <gregor.sokolowski@gmail.com>
2025-09-25 22:14:51 +0200

    something else

---
+++ app/core/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+// in core folder these files will be always loaded for the application
+
+function show($url) {
+    echo '<pre>';
+    print_r($url);
+    echo '</pre>';
+}
--- public/index.php
+++ public/index.php
@@ -1,30 +1,10 @@
 <?php
+
+use App\Core\App;
+
 session_start();
 
-function show($url) {
-    echo '<pre>';
-    print_r($url);
-    echo '</pre>';
-}
+require_once '../app/core/init.php';
 
-function splitUrl() {
-    // Get the URL from the query string or default to home
-    $url = $_GET['url'] ?? 'home';
-    $url = explode('/', $url);
-    return $url;
-}
-
-//show(splitUrl());
-
-function loadController() {
-    $url = splitUrl();
-
-    $controllerFileName = '../app/controllers/' . ucfirst($url[0]) . '.php';
-    if (file_exists($controllerFileName)) {
-    } else {
-        $controllerFileName = '../app/controllers/_404.php';
-    }
-    require_once $controllerFileName;
-}
-
-loadController();
+$app = new App;
+$app->loadController();
