9f1abb03647452d693afc534583f65d9cd048c91
Greg <greg@mac.home>
2025-09-30 15:23:24 +0200

    controller view assets added

--- app/controllers/_404.php
+++ app/controllers/_404.php
@@ -8,6 +8,7 @@ class _404 extends Controller
     
     public function index() {
         echo "404 error - controller not found";
+        $this->view('404');
     }
 }
 // call this class from App.php
--- app/views/products/products.view.php
+++ app/views/products/products.view.php
@@ -1,1 +1,2 @@
 <h1>Products view file</h1>
+<img src="<?= ROOT ?>/assets/images/product.png" width="200px">
