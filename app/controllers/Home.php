8fbe5722c24a8d280947070cdcd942212bd9b7e4
gsokolowski <gregor.sokolowski@gmail.com>
2025-09-30 14:33:18 +0200

    controller base calles view

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,6 +8,7 @@ class Home extends Controller
     
     public function index() {
         echo "home controller loaded and method index called --";
+        $this->view('Home');
     }
 }
 // call this class from App.php
