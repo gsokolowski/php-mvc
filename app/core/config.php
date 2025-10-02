30fbfb9052d11834ca3ddc2fb370e264a51878ed
gsokolowski <gregor.sokolowski@gmail.com>
2025-10-02 02:28:29 +0200

    Database Class added, db made and first test connecttion made from Home controller through Model

--- app/core/config.php
+++ app/core/config.php
@@ -2,8 +2,22 @@
 // constants are accessible all over the application
 
 if($_SERVER['SERVER_NAME'] == 'localhost') {
+    
+    // Database credentials
+    define('DB_NAME', 'php-mvc');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+
     // Root for images and other asset files based in public folder
     define('ROOT', 'http://localhost/garbage/php-mvc/public'); 
 }else {
+
+    // Database credentials
+    define('DB_NAME', 'php-mvc');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+
     define('ROOT', 'https://www.yourdomain.com/public'); 
 }
